bed4c848ca13b82395127eac6d7b5efb4c435907
John Comeau <jc@unternet.net>
2022-06-12 15:08:34 -0700

    fixed to download bootstrap from my own server

--- app/src/main/java/com/termux/app/TermuxInstaller.java
+++ app/src/main/java/com/termux/app/TermuxInstaller.java
@@ -170,7 +170,7 @@ final class TermuxInstaller {
     /** Get bootstrap zip url for this systems cpu architecture. */
     private static URL determineZipUrl() throws MalformedURLException {
         String archName = determineTermuxArchName();
-        String url = "https://termux.net/bootstrap/new/bootstrap-" + archName + ".zip";
+        String url = "https://termux.unternet.net/bootstrap/new/bootstrap-" + archName + ".zip";
         return new URL(url);
     }
 
